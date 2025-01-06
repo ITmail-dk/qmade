0c73ca12a9e7128e0718b20d77ddf283a84b10de
ITmail-dk <48917593+ITmail-dk@users.noreply.github.com>
2025-01-06 12:56:27 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1463,11 +1463,12 @@ cat << "QTILECONFIG" > ~/.config/qtile/config.py
 #
 import os
 import subprocess
+import json
 from libqtile import bar, layout, qtile, widget, hook
 from libqtile.config import Click, Drag, Group, Key, Match, Screen, ScratchPad, DropDown, re
 from libqtile.lazy import lazy
 from libqtile.utils import guess_terminal # terminal = guess_terminal()
-from qtile_colors import colors
+#from qtile_colors import colors
 #from libqtile.dgroups import simple_key_binder
 
 
@@ -1494,9 +1495,26 @@ def guess_browser():
 # Custom code end ------------------------------------------------------
 
 #Pywal Colors
-#color = os.path.expanduser('~/.cache/wal/colors.json')
+colors = os.path.expanduser("~/.cache/wal/colors.json")
+colordict = json.load(open(colors))
+Color0 = colordict["colors"]["color0"]
+Color1 = colordict["colors"]["color1"]
+Color2 = colordict["colors"]["color2"]
+Color3 = colordict["colors"]["color3"]
+Color4 = colordict["colors"]["color4"]
+Color5 = colordict["colors"]["color5"]
+Color6 = colordict["colors"]["color6"]
+Color7 = colordict["colors"]["color7"]
+Color8 = colordict["colors"]["color8"]
+Color9 = colordict["colors"]["color9"]
+Color10 = colordict["colors"]["color10"]
+Color11 = colordict["colors"]["color11"]
+Color12 = colordict["colors"]["color12"]
+Color13 = colordict["colors"]["color13"]
+Color14 = colordict["colors"]["color14"]
+Color15 = colordict["colors"]["color15"]
 
-# Colors use example active=color["3"],
+# Colors use example active=Color1,
 
 
 mod = "mod4"
