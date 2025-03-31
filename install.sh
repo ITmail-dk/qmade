2845a4b4b76363d70b940a4343326b88d5ce8953
mara <post@itmail.dk>
2025-03-31 13:35:20 +0200

    add end install

--- install.sh
+++ install.sh
@@ -1656,7 +1656,7 @@ check_error "Kitty config file"
 sudo sed -i 's+GRUB_TIMEOUT=5+GRUB_TIMEOUT=1+g' /etc/default/grub && sudo update-grub
 check_error "GRUB BOOT TIMEOUT"
 
-# Install selection choose what to install
+# End install selection choose what to install
 for PROGRAM in $PROGRAMS
 do
     case $PROGRAM in
