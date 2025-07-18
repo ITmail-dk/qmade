e4811ec9f93c25efb468685a41b7b3cb748f989b
Martin Andersen <post@itmail.dk>
2025-07-18 11:30:48 +0200

    MODE boot logo

--- install.sh
+++ install.sh
@@ -192,8 +192,7 @@ function start_installation() {
 
   # -------------------------------------------------------------------------------------------------
   # Core System APT install
-  sudo DEBIAN_FRONTEND=noninteractive apt -y --ignore-missing install bash-completion xserver-xorg x11-utils xinit acl arandr autorandr picom fwupd colord mesa-utils htop wget curl git tmux numlockx kitty neovim xdg-utils cups cups-common lm-sensors fancontrol xbacklight brightnessctl unzip network-manager dnsutils dunst libnotify-bin notify-osd xsecurelock pm-utils rofi 7zip jq poppler-utils fd-find ripgrep zoxide imagemagick nsxiv mpv flameshot mc thunar gvfs gvfs-backends parted gparted mpd mpc ncmpcpp fzf ccrypt xarchiver notepadqq font-manager fontconfig fontconfig-config fonts-recommended fonts-liberation fonts-freefont-ttf fonts-noto-core libfontconfig1 pipewire pipewire-audio pipewire-alsa pipewire-pulse pipewire-jack wireplumber libspa-0.2-bluetooth pavucontrol playerctl alsa-utils qpwgraph sddm-theme-breeze sddm-theme-maui ffmpeg cmake remmina libreoffice linux-cpupower plymouth plymouth-theme-hamara
-
+  sudo DEBIAN_FRONTEND=noninteractive apt -y --ignore-missing install bash-completion xserver-xorg x11-utils xinit acl arandr autorandr picom fwupd colord mesa-utils htop wget curl git tmux numlockx kitty neovim xdg-utils cups cups-common lm-sensors fancontrol xbacklight brightnessctl unzip network-manager dnsutils dunst libnotify-bin notify-osd xsecurelock pm-utils rofi 7zip jq poppler-utils fd-find ripgrep zoxide imagemagick nsxiv mpv flameshot mc thunar gvfs gvfs-backends parted gparted mpd mpc ncmpcpp fzf ccrypt xarchiver notepadqq font-manager fontconfig fontconfig-config fonts-recommended fonts-liberation fonts-freefont-ttf fonts-noto-core libfontconfig1 pipewire pipewire-audio pipewire-alsa pipewire-pulse pipewire-jack wireplumber libspa-0.2-bluetooth pavucontrol playerctl alsa-utils qpwgraph sddm-theme-breeze sddm-theme-maui ffmpeg cmake remmina libreoffice linux-cpupower plymouth
   # For packages that might be missing so it doesn't stop the big apt installation of packages or slow it down
   for i in policykit-1 policykit-1-gnome keynav yt-dlp; do
     sudo DEBIAN_FRONTEND=noninteractive apt -y --ignore-missing install $i
@@ -1837,7 +1836,7 @@ EOF
   sudo update-grub
 
   # List boot themes run: sudo plymouth-set-default-theme -l
-  sudo plymouth-set-default-theme -R hamara
+  sudo plymouth-set-default-theme -R tribar 
 
   clear #Clear the screen
   check_error "GRUB BOOT TIMEOUT AND SPLASH"
