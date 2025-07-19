518a49002e82e0671e6233c2dabc5b6c3e8021e1
Martin Andersen <post@itmail.dk>
2025-07-19 15:28:11 +0200

    FIX GRUB quiet splash

--- install.sh
+++ install.sh
@@ -1499,7 +1499,7 @@ EOF
 
   # Edit GRUB BOOT TIMEOUT AND SPLASH
   sudo sed -i 's+GRUB_TIMEOUT=5+GRUB_TIMEOUT=1+g' /etc/default/grub
-  sudo sed -i '0,/"quiet"/ s/"quiet"/ "quiet splash"/' /etc/default/grub
+  sudo sed -i '0,/"quiet"/ s/"quiet"/"quiet splash"/' /etc/default/grub
   sudo update-grub
 
   # List boot themes run: sudo plymouth-set-default-theme -l
